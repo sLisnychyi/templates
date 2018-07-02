31625d9670365da2961bbcf07b0f916b2b3138de
lisnychyis <ljore@ukr.net>
2018-07-02 21:31:28 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import {MatTableModule, MatButtonModule, MatCheckboxModule} from "@angular/material";
+import { AppRoutingModule }     from './app-routing.module';
+import {DataTablesModule} from "angular-datatables";
+import {HttpClientModule} from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { MessagesComponent }    from './messages/messages.component';
+import { UsertableComponent } from './usertable/usertable.component';
+
+import {UserService} from "./services/user.service"
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    MatTableModule, MatButtonModule, MatCheckboxModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    UsertableComponent
+  ],
+  providers: [UserService],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
